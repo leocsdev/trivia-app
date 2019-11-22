b81ffd2f8a2ded79b02b6408708a08990db49bf8
Leo Santillan <leocsdev@gmail.com>
2019-11-22 15:48:04 +0800

    Created components and layout folders.

---
+++ src/components/Trivia.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Trivia extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Trivia Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Trivia;
